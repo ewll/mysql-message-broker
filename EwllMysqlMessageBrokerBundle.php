c780dbc5ec4621aa5e4c3e05f3795a525d64848d
Maxim Salamakhin <343604@gmail.com>
2019-08-26 21:06:59 +0700

    init

---
+++ EwllMysqlMessageBrokerBundle.php
@@ -0,0 +1,10 @@
+<?php namespace Ewll\MysqlMessageBrokerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * {@inheritdoc}
+ */
+class EwllMysqlMessageBrokerBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Ewll/MysqlMessageBrokerBundle
